944675f9784298be63fd17ceacb4a27536fe223b
furqan78 <furqangithub2@gmail.com>
2023-12-28 15:23:50 +0530

    Fix: Made responsive changes and add blog section

--- index.js
+++ index.js
@@ -14,24 +14,24 @@ mobile_nav.addEventListener("click", () => toggleNavbar());
 // ========================================
 // sticky navigation
 // ========================================
-const sectionHero = document.querySelector(".section-hero");
+const sectionHero = document.querySelector(".main-section");
 
-const observer = new IntersectionObserver(
-    (entries) => {
-        const ent = entries[0];
-        console.log(ent);
-        !ent.isIntersecting ?
-            document.body.classList.add("sticky") :
-            document.body.classList.remove("sticky");
-    }, {
-        // viewport
-        root: null,
-        threshold: 0,
-        rootMargin: "-100px",
-    }
-);
+// const observer = new IntersectionObserver(
+//     (entries) => {
+//         const ent = entries[0];
+//         console.log(ent);
+//         !ent.isIntersecting ?
+//             document.body.classList.add("sticky") :
+//             document.body.classList.remove("sticky");
+//     }, {
+//         // viewport
+//         root: null,
+//         threshold: 0,
+//         rootMargin: "-100px",
+//     }
+// );
 // when the hero section end part reached then we need to show the sticky navigation
-observer.observe(sectionHero);
+// observer.observe(sectionHero);
 
 // ========================================
 //  how to add media queries in JS
@@ -83,13 +83,14 @@ const scrollElement = document.createElement("div");
 scrollElement.classList.add("scrollTop-style");
 
 // I am adding the button element inside the div element
-scrollElement.innerHTML = ` <ion-icon name="arrow-up-outline" class="scroll-top"></ion-icon>`;
+scrollElement.innerHTML = `<ion-icon name="arrow-up-outline" class="scroll-top"></ion-icon>`;
 
 // add to the bottom of the page
 footerElm.after(scrollElement);
 
 // deleting the dom element
 const scrollTop = () => {
+    console.log('getting scroll function');
     sectionHero.scrollIntoView({ behavior: "smooth" });
 };
 
@@ -108,11 +109,13 @@ const contactSection = document.querySelector(".section-contact");
 //     e.preventDefault();
 //     portfolioSection.scrollIntoView({ behavior: "smooth" });
 // });
-
-document.querySelector(".hireme-btn").addEventListener("click", (e) => {
+const hiremeBtn = document.querySelector(".hireme-btn");
+if(hiremeBtn){
+hiremeBtn.addEventListener("click", (e) => {
     e.preventDefault();
     contactSection.scrollIntoView({ behavior: "smooth" });
 });
+}
 
 // ========================================
 // creating a portfolio tabbed component
@@ -158,67 +161,164 @@ const img_div = document.querySelectorAll(".img-ovelay");
 // ========================================
 //  lazy loading section
 // ========================================
-const imgRef = document.querySelector("img[data-src]");
-console.log(imgRef);
+// const imgRef = document.querySelector("img[data-src]");
+// console.log(imgRef);
 
-const lazyImg = (entries) => {
-    const [entry] = entries;
-    if (!entry.isIntersecting) return;
-    entry.target.src = imgRef.dataset.src;
-};
+// const lazyImg = (entries) => {
+//     const [entry] = entries;
+//     if (!entry.isIntersecting) return;
+//     entry.target.src = imgRef.dataset.src;
+// };
 
-const imgObserver = new IntersectionObserver(lazyImg, {
-    root: null,
-    threshold: 0,
-    // rootMargin: "100px",
-});
+// const imgObserver = new IntersectionObserver(lazyImg, {
+//     root: null,
+//     threshold: 0,
+//     // rootMargin: "100px",
+// });
 
-imgObserver.observe(imgRef);
+// imgObserver.observe(imgRef);
 
 // ========================================
 //  animated counter number
 // ========================================
 
-const workSection = document.querySelector(".section-work-data");
+// const workSection = document.querySelector(".section-work-data");
 
-const workSectionObserve = (entries) => {
-    const [entry] = entries;
-    if (!entry.isIntersecting) return;
-    console.log(entries);
+// const workSectionObserve = (entries) => {
+//     const [entry] = entries;
+//     if (!entry.isIntersecting) return;
+//     console.log(entries);
 
 
-    const counterNum = document.querySelectorAll(".counter-numbers");
-    // console.log(counterNum);
-    const speed = 10;
+//     const counterNum = document.querySelectorAll(".counter-numbers");
+//     // console.log(counterNum);
+//     const speed = 10;
 
-    counterNum.forEach((curNumber) => {
-        const updateNumber = () => {
-            const targetNumber = parseInt(curNumber.dataset.number);
-            // console.log(targetNumber);
-            const initialNumber = parseInt(curNumber.innerText);
-            // console.log(initialNumber);
-            const incrementNumber = Math.trunc(targetNumber / speed);
-            // i am adding the value to the main number
-            // console.log(incrementNumber);
+//     counterNum.forEach((curNumber) => {
+//         const updateNumber = () => {
+//             const targetNumber = parseInt(curNumber.dataset.number);
+//             // console.log(targetNumber);
+//             const initialNumber = parseInt(curNumber.innerText);
+//             // console.log(initialNumber);
+//             const incrementNumber = Math.trunc(targetNumber / speed);
+//             // i am adding the value to the main number
+//             // console.log(incrementNumber);
 
-            if (initialNumber < targetNumber) {
-                curNumber.innerText = `${initialNumber + incrementNumber}+`;
-                setTimeout(updateNumber, 120);
-            } else {
-                curNumber.innerText = `${targetNumber}+`;
-            }
+//             if (initialNumber < targetNumber) {
+//                 curNumber.innerText = `${initialNumber + incrementNumber}+`;
+//                 setTimeout(updateNumber, 120);
+//             } else {
+//                 curNumber.innerText = `${targetNumber}+`;
+//             }
 
-        };
-        updateNumber();
-    });
-};
+//         };
+//         updateNumber();
+//     });
+// };
 
-const workSecObserver = new IntersectionObserver(workSectionObserve, {
-    root: null,
-    threshold: 0,
-});
+// const workSecObserver = new IntersectionObserver(workSectionObserve, {
+//     root: null,
+//     threshold: 0,
+// });
 
-workSecObserver.observe(workSection);
+// workSecObserver.observe(workSection);
+
+var i = 0;
+var j = 0;
+var txt1 = 'Ali Furqan';
+var txt2 = 'A Freelancer';
+var txt3 = 'A Developer';
+var txt4 = 'A Youtuber';
+var mainText = ['Ali Furqan', 'A Freelancer', 'A ', 'A Youtuber'];
+var speed = 70;
+
+const textChange = document.getElementById("heading-txt-change");
+
+if(textChange){
+        typeWriter();
+}
+
+// if (textChange) {
+//     for (let i = 0; i < 4; i++) {
+//         setTimeout(() => {
+         
+//             switch (true) {
+//                 case i == 0:
+//                     textChange.innerHTML = '';
+//                     mainText = txt1;
+//                     typeWriter();
+//                     break;
+//                 case i == 1:
+//                     textChange.innerHTML = '';
+//                     mainText = txt2;
+//                     typeWriter();
+//                     break;
+//                 case i == 2:
+//                     textChange.innerHTML = '';
+//                     mainText = txt3;
+//                     typeWriter();
+//                     break;
+//                 case i == 3:
+//                     textChange.innerHTML = '';
+//                     mainText = txt4;
+//                     typeWriter();
+//                     break;
+//             }
+//         }, 2000);
+//     }
+// }
+
+
+
+function typeWriter() {
+    if (mainText[j]) {
+        console.log(mainText[j], 'getting main texrt');
+        if (i < mainText[j].length) {
+            textChange.innerHTML += mainText[j].charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        } else {
+            // textChange.innerHTML = '';
+            // if(j <= mainText.length){
+            //     j = 0;
+            // }else{
+            //     j++;
+            // }
+        }
+    }
+}
+
+// function typeWriter() {
+//     for (let i = 0; i < 5; i++) {
+//         switch (true) {
+//             case i == 1:
+//                 document.getElementById("heading-txt-change").innerHTML = '';
+//                 mainText = txt1;
+//                 break;
+
+//             case i == 2:
+//                 document.getElementById("heading-txt-change").innerHTML = '';
+//                 mainText = txt2;
+//                 break;
+
+//             case i == 3:
+//                 document.getElementById("heading-txt-change").innerHTML = '';
+//                 mainText = txt3;
+//                 break;
+
+//             case i == 4:
+//                 document.getElementById("heading-txt-change").innerHTML = '';
+//                 mainText = txt4;
+//                 break;
+//         }
+
+//         for (let j = 0; j < mainText.length; j++) {
+//             setTimeout(() => {
+//                 document.getElementById("heading-txt-change").innerHTML += mainText.charAt(j);
+//             }, speed);
+//         }
+//     }
+// }
 
 function validate() {
     var username = document.getElementById('username').value;
