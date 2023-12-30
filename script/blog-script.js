cc88f0d8a6405b7cc466b19422884a064d3bb00e
furqan78 <furqangithub2@gmail.com>
2023-12-30 14:53:03 +0530

    Feat: Add google ads code and blog section

---
+++ script/blog-script.js
@@ -0,0 +1,50 @@
+fetch('../constant/blog-data.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data, ' getting blog data');
+    
+let list = document.getElementById("blog-main-container");
+for (i = 0; i < data.length; ++i) {
+    let div = document.createElement('div');
+    div.classList.add('blog-container');
+
+    let heading = document.createElement('h2');
+    heading.classList.add('blog-title');
+    heading.innerHTML = data[i].title;
+
+    let div2 = document.createElement('div');
+    div2.classList.add('publish-author-container');
+
+    let img = document.createElement('img');
+    img.classList.add('author-img');
+    img.src = data[i].author_img;
+
+    let para = document.createElement('p');
+    para.classList.add('publish-para');
+
+    let span = document.createElement('span');
+    span.classList.add('blog-author-para');
+    span.innerHTML = data[i].author;
+    para.appendChild(span);
+    para.innerHTML = para.innerHTML + ' - ' + data[i].publish_date;
+
+    div2.appendChild(img);
+    div2.appendChild(para);
+
+    let contentPara = document.createElement('p');
+    contentPara.classList.add('blog-content');
+    let linkTag = document.createElement('a');
+    linkTag.classList.add('read-more-btn');
+    linkTag.innerHTML = 'Read More';
+    linkTag.href = data[i].redirect_path;
+    contentPara.innerHTML = data[i].content.slice(0,200) + '... ';
+    contentPara.appendChild(linkTag);
+    // let div1 = document.createElement('div');
+    // div1.classList.add('publish-author-container');
+    div.appendChild(heading);
+    div.appendChild(div2);
+    div.appendChild(contentPara);
+    list.appendChild(div);
+}
+  })
+  .catch(error => console.error('Error fetching JSON:', error));
